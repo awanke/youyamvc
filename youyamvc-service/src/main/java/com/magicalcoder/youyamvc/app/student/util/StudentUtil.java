e103917c8047fefd64a1e91a2ae0127b0db0aa4c
hedongyu <554151811@qq.com>
2016-04-26 17:50:20 +0800

    新API

--- youyamvc-service/src/main/java/com/magicalcoder/youyamvc/app/student/util/StudentUtil.java
+++ youyamvc-service/src/main/java/com/magicalcoder/youyamvc/app/student/util/StudentUtil.java
@@ -1,6 +1,7 @@
 package com.magicalcoder.youyamvc.app.student.util;
 /**
 * Created by www.magicalcoder.com
+* 如果你改变了此类 read 请将此行删除
 * 799374340@qq.com
 */
 public class StudentUtil {
@@ -12,13 +13,36 @@ public class StudentUtil {
             orderBySqlField = orderBySqlField.toLowerCase().trim();
             descAsc=descAsc.toLowerCase().trim();
             if("asc".equals(descAsc) || "desc".equals(descAsc)){
-                    String orderBySqlFieldStr = ",class_id,sex,name,";
+                    String orderBySqlFieldStr = ",class_id,sex,name,identy_key,";
                 if(orderBySqlFieldStr.contains("" + orderBySqlField+"")){//精确匹配可排序字段
                     orderBy = orderBySqlField+" "+descAsc;
                 }
             }
         }
         return orderBy;
+    }
+
+    //排序部分防sql注入安全过滤
+    public static String filterOrderBy(String orderByStr){
+        StringBuilder sb = new StringBuilder();
+        if(orderByStr!=null && !"".equals(orderByStr.trim())){
+            orderByStr = orderByStr.toLowerCase().trim();
+            String[] arr = orderByStr.split(",");
+            for(String sqlOrderField:arr){
+                String orderBySqlFieldStr = ",class_id,sex,name,identy_key,";
+                String sqlField = sqlOrderField.replaceAll("\\s+asc|\\s+desc","").trim();
+                if(!"".equals(sqlField)&&orderBySqlFieldStr.contains(sqlField)){//精确匹配可排序字段
+                    sb.append(sqlOrderField).append(",");
+                }
+            }
+        }
+        if("".equals(sb.toString())){
+            return null;
+        }
+        if(sb.toString().endsWith(",")){
+            return sb.substring(0,sb.length()-1);
+        }
+        return null;
     }
 
 }
--- youyamvc-web/src/main/java/com/magicalcoder/youyamvc/web/controller/web/student/StudentListController.java
+++ youyamvc-web/src/main/java/com/magicalcoder/youyamvc/web/controller/web/student/StudentListController.java
@@ -1,4 +1,5 @@
 package com.magicalcoder.youyamvc.web.controller.web.student;
+
 import com.magicalcoder.youyamvc.app.student.service.StudentService;
 import com.magicalcoder.youyamvc.app.student.constant.StudentConstant;
 import com.magicalcoder.youyamvc.app.student.util.StudentUtil;
@@ -14,11 +15,13 @@ import com.magicalcoder.youyamvc.web.common.BaseController;
 import com.magicalcoder.youyamvc.app.userweb.util.UserWebUtil;
 
 import org.springframework.format.annotation.DateTimeFormat;
+
 import java.util.*;
 import java.math.*;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -28,87 +31,87 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
-* Created by www.magicalcoder.com
-* 如果你改变了此类 read 请将此行删除
-* 799374340@qq.com
-*/
+ * Created by www.magicalcoder.com
+ * 如果你改变了此类 read 请将此行删除
+ * 799374340@qq.com
+ */
 @RequestMapping({"/web/student"})
 @Controller
-public class StudentListController extends BaseController
-{
+public class StudentListController extends BaseController {
     @Resource
     private StudentService studentService;
-/**  method:GET
-     *   url:/user/student/list/{pageIndex}/{pageSize}?...
-     *   demo:/user/student/list/1/20
-     *   获取学生分页数据
-     *   是否需要登录
-     *是否必须   入参                          注释
-     *  是   @pageIndex                     (Integer)当前页码 [1,n]
-     *  是   @pageSize                      (Integer)每页条数 [1,n]
-     *  否   @pageCount                     (Integer)页数 如果传进来将会优化性能 对于需要分页的数据 请求第二页的时候可以把上一页的值传进来
-     *  否   @needPageCount                 (Boolean)是否需要返回pageCount
-     *  否   @callback                      (String)回调方法
-     *  否   @encode                        (String)编码 默认UTF-8
-     *  否   @classId                     (Long)所属班级 
-     *  否   @sex                     (Integer)性别 [{"":"全部"},{"0":"类型一"},{"1":"类型二"}]
-     *  否   @name                     (String)学生名称 
-     *  否   @identyKey                     (Long)主键值 
-     *  否   @classIdFirst                     (Long)所属班级 
-     *  否   @sexFirst                     (Integer)性别 [{"":"全部"},{"0":"类型一"},{"1":"类型二"}]
-     *  否   @nameFirst                     (String)学生名称 
-     *  否   @orderBySqlField                     (String)排序字段 class_id|sex|name|identy_key|
-     *  否   @descAsc                             (String)desc|asc
-     *   返回
-     *   {
-     *      code:0,message:"",jsonp:"",
-     *      info:
-                pageCount://总数目
-                pageList:[{
-     *              classId         (Long)所属班级 
-     *              sex         (Integer)性别 [{"":"全部"},{"0":"类型一"},{"1":"类型二"}]
-     *              name         (String)学生名称 
-     *              identyKey         (Long)主键值 
-     *      }]
-     *   }
+
+    /**
+     * method:GET
+     * url:/web/student/list/{pageIndex}/{pageSize}?...
+     * demo:/web/student/list/1/20
+     * 获取学生分页数据
+     * 是否需要登录
+     * 是否必须   入参                          注释
+     * 是   @pageIndex                     (Integer)当前页码 [1,n]
+     * 是   @pageSize                      (Integer)每页条数 [1,n]
+     * 否   @pageCount                     (Integer)页数 如果传进来将会优化性能 对于需要分页的数据 请求第二页的时候可以把上一页的值传进来
+     * 否   @needPageCount                 (Boolean)是否需要返回pageCount
+     * 否   @callback                      (String)回调方法
+     * 否   @orderBy                       (String)排序字段
+     * 否   @encode                        (String)编码 默认UTF-8
+     * 否   @modelView                     (String)模板方法 vm|json
+     * 否   @classId                     (Long)所属班级
+     * 否   @sex                     (Integer)性别 [{"":"全部"},{"0":"类型一"},{"1":"类型二"}]
+     * 否   @name                     (String)学生名称
+     * 否   @identyKey                     (Long)主键值
+     * 否   @classIdFirst                     (Long)所属班级
+     * 否   @sexFirst                     (Integer)性别 [{"":"全部"},{"0":"类型一"},{"1":"类型二"}]
+     * 否   @nameFirst                     (String)学生名称
+     * 返回
+     * {
+     * code:0,message:"",jsonp:"",
+     * info:
+     * pageCount://总数目
+     * pageList:[{
+     * classId         (Long)所属班级
+     * sex         (Integer)性别 [{"":"全部"},{"0":"类型一"},{"1":"类型二"}]
+     * name         (String)学生名称
+     * identyKey         (Long)主键值
+     * }]
+     * }
      */
-    @RequestMapping(value={"list/{pageIndex}/{pageSize}"}, method={RequestMethod.GET})
-    public void list(@PathVariable Integer pageIndex,@PathVariable Integer pageSize,
-        @RequestParam(required=false, value="orderBySqlField") String orderBySqlField,
-        @RequestParam(required=false, value="descAsc") String descAsc,
-        @RequestParam(required=false, value="pageCount") Integer pageCount,
-        @RequestParam(required=false, value="needPageCount") Boolean needPageCount,
-        @RequestParam(required=false, value="callback") String callback,
-        @RequestParam(required=false, value="encode") String encode,
-        @RequestParam(required = false,value ="classId")   Long classId,
-        @RequestParam(required = false,value ="sex")   Integer sex,
-        @RequestParam(required = false,value ="name")   String name,
-        @RequestParam(required = false,value ="identyKey")   Long identyKey,
-        @RequestParam(required = false,value ="classIdFirst")                        Long classIdFirst ,
-        @RequestParam(required = false,value ="sexFirst")                        Integer sexFirst ,
-        @RequestParam(required = false,value ="nameFirst")                        String nameFirst ,
-        HttpServletRequest request,HttpServletResponse response)
-    {
-        String orderBy = StudentUtil.filterOrderBy(orderBySqlField,descAsc);
-        pageSize = Math.min(StudentConstant.PAGE_MAX_SIZE,pageSize);
+    @RequestMapping(value = {"list/{pageIndex}/{pageSize}"}, method = {RequestMethod.GET})
+    public String list(@PathVariable Integer pageIndex, @PathVariable Integer pageSize,
+                       @RequestParam(required = false, value = "orderBy") String orderBy,
+                       @RequestParam(required = false, value = "pageCount") Integer pageCount,
+                       @RequestParam(required = false, value = "needPageCount") Boolean needPageCount,
+                       @RequestParam(required = false, value = "callback") String callback,
+                       @RequestParam(required = false, value = "encode") String encode,
+                       @RequestParam(required = false, value = "classId") Long classId,
+                       @RequestParam(required = false, value = "sex") Integer sex,
+                       @RequestParam(required = false, value = "name") String name,
+                       @RequestParam(required = false, value = "identyKey") Long identyKey,
+                       @RequestParam(required = false, value = "classIdFirst") Long classIdFirst,
+                       @RequestParam(required = false, value = "sexFirst") Integer sexFirst,
+                       @RequestParam(required = false, value = "nameFirst") String nameFirst,
+                       @RequestParam(required = false, value = "modelView") String modelView,
+                       HttpServletRequest request, HttpServletResponse response) {
+        orderBy = StudentUtil.filterOrderBy(orderBy);
+        pageSize = Math.min(StudentConstant.PAGE_MAX_SIZE, pageSize);
         int idx = (pageIndex.intValue() - 1) * pageSize;
-     Long userId = UserWebUtil.userId(request);
+        Long userId = UserWebUtil.userId(request);
 
 
-     Map<String,Object> query = ProjectUtil.buildMap("orderBy", orderBy, new Object[] {
-         "classId",classId,
-         "sex",sex,
-         "name",name,
-         "identyKey",identyKey,
-                "classIdFirst",classIdFirst ,
-                "sexFirst",sexFirst ,
-                "nameFirst",nameFirst ,
-        "limitIndex",idx,"limit", pageSize ,"userId",userId});
+        Map<String, Object> query = ProjectUtil.buildMap("orderBy", orderBy, new Object[]{
+                "classId", classId,
+                "sex", sex,
+                "name", name,
+                "identyKey", identyKey,
+                "classIdFirst", classIdFirst,
+                "sexFirst", sexFirst,
+                "nameFirst", nameFirst,
+                "limitIndex", idx, "limit", pageSize, "userId", userId});
 
         List<Student> pageList = this.studentService.getStudentList(query);
         Map ajaxData = new HashMap();
         ajaxData.put("pageList", pageList);
-        if(needPageCount!=null && needPageCount){
+        if (needPageCount != null && needPageCount) {
             if (pageCount == null || pageCount.intValue() == 0) {
                 query.remove("orderBy");
                 query.remove("limitIndex");
@@ -117,55 +120,75 @@ public class StudentListController extends BaseController
             }
             ajaxData.put("pageCount", pageCount);
         }
-        toWebSuccessJson(response, callback,encode,ajaxData);
-    }
-    private Student getEntity(Long userId,Long identyKey){
-        return studentService.selectOneStudentWillThrowException(ProjectUtil.buildMap("identyKey",identyKey,"userId",userId));
-    }
-    private Map<String,Object> toMap(Student entity){
-        Map<String,Object> map = new HashMap<String, Object>();
-        map.put("classId",entity.getClassId());
-        map.put("sex",entity.getSex());
-        map.put("name",entity.getName());
-        map.put("identyKey",entity.getIdentyKey());
-        return map;
-    }
-    /** method:GET
-    *   url:/user/student/get/{identyKey}
-    *   demo:/user/student/get/1
-    *   根据主键获取学生
-    *是否必须         入参                              注释
-    *是       @identyKey               (Long)主键
-    *
-    *   返回
-    *   {
-    *      code:0,message:"",jsonp:"",
-    *      info:{
-    *              classId         (Long)所属班级 
-    *              sex         (Integer)性别 [{"":"全部"},{"0":"类型一"},{"1":"类型二"}]
-    *              name         (String)学生名称 
-    *              identyKey         (Long)主键值 
-    *      }
-    *   }
-    */
-    @RequestMapping(value = "/get/{identyKey}",method = RequestMethod.GET)
-    public void get(@PathVariable Long identyKey,
-        HttpServletRequest request,HttpServletResponse response){
-        Long userId = UserWebUtil.userId(request);
-        Student entity = studentService.selectOneStudentWillThrowException(ProjectUtil.buildMap("identyKey",identyKey,"userId",userId));
-        toWebSuccessJson(response, toMap(entity));
+        if ("vm".equals(modelView)) {
+            return "web/student/list.vm";
+        } else {
+            toWebSuccessJson(response, callback, encode, ajaxData);
+            return null;
+        }
     }
 
-    @RequestMapping(value = "/testvm",method = RequestMethod.GET)
-    public String testVm(ModelMap modelMap){
-        modelMap.addAttribute("name","");
-        modelMap.addAttribute("StringUtils", StringUtils.class);
-        return "web/student.vm";
+    private Map<String, Object> toMap(Student entity) {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("classId", entity.getClassId());
+        map.put("sex", entity.getSex());
+        map.put("name", entity.getName());
+        map.put("identyKey", entity.getIdentyKey());
+        return map;
     }
-    @RequestMapping(value = "/testjsp",method = RequestMethod.GET)
-    public String testJsp(ModelMap modelMap){
-        modelMap.addAttribute("name","");
-        modelMap.addAttribute("StringUtils",StringUtils.class);
-        return "student";
+
+    /**
+     * method:GET
+     * url:/web/student/item?...
+     * demo:/web/student/item
+     * 获取学生分页数据
+     * 是否需要登录
+     * 是否必须   入参                          注释
+     * 否   @callback                      (String)回调方法
+     * 否   @orderBy                       (String)排序字段
+     * 否   @encode                        (String)编码 默认UTF-8
+     * 否   @modelView                     (String)模板方法 vm|json
+     * 否  @identyKey        (Long) 主键
+     * 否  @name, (String)
+     * 返回
+     * {
+     * code:0,message:"",jsonp:"",
+     * info:
+     * {
+     * classId         (Long)所属班级
+     * sex         (Integer)性别 [{"":"全部"},{"0":"类型一"},{"1":"类型二"}]
+     * name         (String)学生名称
+     * identyKey         (Long)主键值
+     * }
+     * }
+     */
+    @RequestMapping(value = {"item"}, method = {RequestMethod.GET})
+    public String item(
+            @RequestParam(required = false, value = "callback") String callback,
+            @RequestParam(required = false, value = "encode") String encode,
+            @RequestParam(required = false, value = "modelView") String modelView,
+            @RequestParam(required = false, value = "identyKey") Long identyKey,
+            @RequestParam(required = false, value = "name") String name,
+
+            HttpServletRequest request, HttpServletResponse response) {
+        Long userId = UserWebUtil.userId(request);
+        Map<String, Object> query = ProjectUtil.buildMap(
+                "identyKey", identyKey,
+                "name", name,
+                "limitIndex", 0, "limit", 1, "userId", userId
+        );
+
+        List<Student> pageList = this.studentService.getStudentList(query);
+        Student student = null;
+        if (pageList != null && pageList.size() > 0) {
+            student = pageList.get(0);
+        }
+        if ("vm".equals(modelView)) {
+            return "web/student/item.vm";
+        } else {
+            toWebSuccessJson(response, callback, encode, student);
+            return null;
+        }
     }
+
 }
